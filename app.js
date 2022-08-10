b234c31a19d04c5987c9b82929b25f2e32994c24
mohammedalblewi <mohammadalblewi@gmail.com>
2022-08-10 21:04:26 +0300

    This is task js

---
+++ app.js
@@ -0,0 +1,34 @@
+let name = window. prompt("Enter your name: "); alert("Your name is " + name);
+
+
+let gender = window. prompt("Enter your gender"); 
+
+if ((gender !== "male") && (gender !== "female")){
+    window. prompt("Enter your gender");
+}
+
+// }else if ((gender != "male") || (gender !="female")){
+//     alert ("your gender is " + gender);
+// }
+
+
+let age = window. prompt("Enter Your Age : ")
+
+if (age <= 0 ){
+    alert ("Your age not allowed")
+}
+
+
+
+
+
+
+    if ((gender == "male") || (gender == "Male")) {
+    alert ('Mr ' + name) ;
+
+}
+    else ( gender == "female")
+        alert ("Ms "+ name);
+
+
+        let welcome = window. prompt("Do you want to skip the welcoming message ?");
